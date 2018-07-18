e80b017384f687b4538a57e450fd797e8294507f
Panneer Selvam <panneer.smn@outlook.com>
2018-07-18 13:24:40 -0500

    Created algorith directory * added bubble_sort.py file
    Change-Id: I8780f7a5b0af9b29f4f9615d1872b6dc0e29b73b

---
+++ algorithm/bubblesort.py
@@ -0,0 +1,11 @@
+def bubble_sort(reverse=False):
+    thelist = list(map(int, input('Enter an array:').split(" ")))
+    for each1 in range(1, len(thelist)):
+        for each2 in range(len(thelist)-1, 0, -1):
+            if thelist[each2] < thelist[each2-1]:
+                thelist[each2]=thelist[each2]+thelist[each2-1]
+                thelist[each2-1]=thelist[each2]-thelist[each2-1]
+                thelist[each2]=thelist[each2]-thelist[each2-1]
+    return thelist
+
+print (bubble_sort())
