1095e2abbb084a32bc379a3cb1e04f049d68f364
Panneer Selvam <panneer.smn@outlook.com>
2018-08-02 06:02:11 -0500

    fixed insertion sort
    Change-Id: I9042dd4f8c852e3cf38fd1fa6194db9209138c6c

--- algorithm/insertionsort.py
+++ algorithm/insertionsort.py
@@ -1,11 +1,13 @@
-def ist():
-    thelst = list(map(int, input().split()))
-    for each1 in range(1, len(thelst)):
-        for each2 in range(each1-1, -1, -1):
-            if thelst[each1] < thelst[each2]:
-                thelst[each1] = thelst[each1] + thelst[each2]
-                thelst[each2] = thelst[each1] - thelst[each2]
-                thelst[each1] = thelst[each1] - thelst[each2]
-    return thelst
+def insertionsort():
+    brownie = list(map(int, input().split()))
+    for first in range(len(brownie)):
+        ind = first
+        for second in range(first-1, -1 , -1):
+            if brownie[first]< brownie[second]: 
+                ind = second
+            else:
+                break
+        brownie.insert(ind, brownie.pop(first))
+    return brownie
 
-print (ist())
+print (insertionsort())
